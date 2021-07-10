aa13a6a5a5a9405a1b8757f21ee829ca69f5686c
glennanj1 <glennan20@gmail.com>
2021-07-10 00:25:27 -0400

    fixed permitted params and serializer

--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,4 +1,4 @@
 class GenreSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :mal_id, :rank, :title, :url, :image, :type, :episodes, :start_date, :end_date, :members, :score
+  attributes :mal_id, :rank, :title, :url, :image_url, :type, :episodes, :start_date, :end_date, :members, :score
 end
