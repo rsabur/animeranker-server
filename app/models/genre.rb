bbbcd9e9a85603563528eaf29a79057f3518137e
glennanj1 <glennan20@gmail.com>
2021-07-10 11:40:03 -0400

    added validations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,12 +1,25 @@
 class Genre < ApplicationRecord
-    def self.api
-        url = URI("https://jikan1.p.rapidapi.com/top/anime/1/tv")
+
+    validates :mal_id, uniqueness: true
+    validates :rank, presence: true
+    validates :title, presence: true
+    validates :url, presence: true
+    validates :image_url, presence: true
+    validates :kind, presence: true
+    validates :episodes, presence: true
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+
+    
+    def self.api(genre)
+        url = URI("https://jikan1.p.rapidapi.com/top/anime/1/#{genre}")
 
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Get.new(url)
+        #free api so no need to hide key
         request["x-rapidapi-key"] = 'f01f93eb50msh9a484ab00d62557p18864ejsned94d26b18dd'
         request["x-rapidapi-host"] = 'jikan1.p.rapidapi.com'
 
